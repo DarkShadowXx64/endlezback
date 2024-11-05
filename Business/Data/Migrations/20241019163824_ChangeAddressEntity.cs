b5212d645d46aff183319ae608fbf9235648c1bf
rodrigoBojorquez <rbojorquez1620@gmail.com>
2024-11-05 17:21:18 -0500

    migraciones

---
+++ Business/Data/Migrations/20241019163824_ChangeAddressEntity.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Business.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAddressEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Zip",
+                table: "CustomerAddress",
+                newName: "ZipCode");
+
+            migrationBuilder.RenameColumn(
+                name: "StreetDetail",
+                table: "CustomerAddress",
+                newName: "State");
+
+            migrationBuilder.RenameColumn(
+                name: "Street",
+                table: "CustomerAddress",
+                newName: "Neighborhood");
+
+            migrationBuilder.RenameColumn(
+                name: "Number",
+                table: "CustomerAddress",
+                newName: "City");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address1",
+                table: "CustomerAddress",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address2",
+                table: "CustomerAddress",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Residential",
+                table: "CustomerAddress",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Address1",
+                table: "CustomerAddress");
+
+            migrationBuilder.DropColumn(
+                name: "Address2",
+                table: "CustomerAddress");
+
+            migrationBuilder.DropColumn(
+                name: "Residential",
+                table: "CustomerAddress");
+
+            migrationBuilder.RenameColumn(
+                name: "ZipCode",
+                table: "CustomerAddress",
+                newName: "Zip");
+
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "CustomerAddress",
+                newName: "StreetDetail");
+
+            migrationBuilder.RenameColumn(
+                name: "Neighborhood",
+                table: "CustomerAddress",
+                newName: "Street");
+
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "CustomerAddress",
+                newName: "Number");
+        }
+    }
+}
---
+++ Business/Data/Migrations/20241019164915_ImproveEntityConvenions.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Business.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImproveEntityConvenions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "OrderTypes",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "OrderStatus",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ShipmentId",
+                table: "Order",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Category",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShipmentId",
+                table: "Order");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "OrderTypes",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "OrderStatus",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Category",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+    }
+}
